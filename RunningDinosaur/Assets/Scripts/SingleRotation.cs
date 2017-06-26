5b5b8946cae58f998eccd8ba4963327d4864c144
Unknown <gordon.lee.6170@gmail.com>
2017-06-26 15:53:19 -0400

    new stuff

---
+++ RunningDinosaur/Assets/Scripts/SingleRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleRotation : MonoBehaviour {
+    public float rotateSpeed = 80;
+    private Transform trans;
+
+	// Use this for initialization
+	void Start () {
+        trans = this.transform;
+        rotateSpeed *= Random.Range(1.5f, 3.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        trans.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+}
